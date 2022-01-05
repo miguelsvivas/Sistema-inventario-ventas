672211d996f8655e098cbeaeb22682fd87bfa3b8
Miguel <vivasmiguel96@gmail.com>
2022-01-05 13:18:23 -0500

    agregando rutas y laravel collective

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('categories', CategoryController::class)->names('categories');
+Route::resource('providers', ProviderController::class)->names('providers');
+Route::resource('products', ProductController::class)->names('products');
